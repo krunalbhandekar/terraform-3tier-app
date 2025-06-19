ff03d231bfa5cfb5a2fcb515e85e0d92b89c946f
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-06-19 16:06:20 +0530

    fix(terraform): Fixed s3 terraform file

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,22 +7,6 @@ resource "aws_s3_bucket" "frontend" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "frontend_public" {
-  bucket = aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.frontend.arn}/*"
-    }]
-  })
-
-}
-
 resource "aws_s3_bucket_website_configuration" "frontend_website" {
   bucket = aws_s3_bucket.frontend.id
 
@@ -51,6 +35,23 @@ resource "aws_s3_bucket_public_access_block" "frontend_pab" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_public" {
+  depends_on = [aws_s3_bucket_public_access_block.frontend_pab]
+  bucket     = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
+
 }
 
 # Upload static files
